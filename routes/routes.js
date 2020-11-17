8c5e6f04824256a72dcc603eea34552d009713d3
Mayank Arora <mayank4116@gmail.com>
2020-11-18 02:32:43 +0530

    minor changes

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("this is chatting app");
-})
-
-router.get('/getinfo', (req, res) => {
-    res.send("information");
-})
-
-router.post('/random', (req, res) => {
-    res.send("got it");
+    res.send("this is chatting app server");
 })
 
 module.exports = router;
